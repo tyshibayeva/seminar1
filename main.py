ec2957df49790ca2a94490ffebf50ba226bb0be2
ubuntu <tyshibayeva@gmail.com>
2019-02-08 06:11:03 -0500

    Ainagul

---
+++ main.py
@@ -0,0 +1,15 @@
+from myclass import car
+
+car1 = car()
+car1.disp_inf()
+
+car2 = car()
+car2.types = "sedan"
+car2.label = "nissan"
+car2.color = "red"
+car2.disp_inf()
+
+
+
+
+ 
---
+++ myclass.py
@@ -0,0 +1,18 @@
+class car:
+    types = "liftback"
+    label = "skoda"
+    color = "black"
+    def disp_inf(self):
+       print ("types", self.types)
+       print ("label", self.label)
+       print ("color", self.color)
+              
+#car1 = car()
+#car1.disp_inf()
+
+#car2 = car()
+#car2.types = "sedan"
+#car2.label = "nissan"
+#car2.color = "red"
+#car2.disp_inf()
+          

dfc48695e3af7ff3c201001a210039fa15f014a8
ubuntu <tyshibayeva@gmail.com>
2019-02-15 10:29:45 -0500

    TAB

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from myclass import car
+from myclass import Car # import klassa
 
-car1 = car()
-car1.disp_inf()
-
-car2 = car()
-car2.types = "sedan"
-car2.label = "nissan"
-car2.color = "red"
-car2.disp_inf()
+car1 = Car()   #sozdanie obekt car1 iz classa Car
+car1.disp_inf() #vivodim informaciyy(znacheniya atribut)
 
 
+types = "sedan" #sozdaem peremennu types so znach="sedan"
+label = "nissan"
+color = "red"
+car1.change_inf(types, label,color) #novaya funkciya izmenyauchaya znacheniya tekuchih atributov na novie
+car1.disp_inf() #vivodim informaciyy(znacheniya atribut)
 
 
  
